1e03b037cef4cdf2771cdbd6ad4948dc026725c4
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-01-09 20:05:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header/header' ;
+import Navbar from './components/navbar/navbar' ;
+import About from './components/about/about' ;
+import Experience from './components/experience/experience' ;
+import Services from './components/services/services' ;
+import Portfolio from './components/portfolio/portfolio' ;
+import Testimonials from './components/testimonials/testimonials' ;
+import Contact from './components/contact/contact' ;
+import Footer from './components/footer/footer' ;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react' ;
+import './footer.css' ;
+
+const footer = () => {
+    return (
+        <div>footer
+            <a href="https://www.freepik.com/free-vector/wave-textures-white-background-vector_3428189.htm#query=texture%20background&position=1&from_view=keyword">Image by rawpixel.com</a> on Freepik
+        </div>
+    )
+}
+
+export default footer ;
