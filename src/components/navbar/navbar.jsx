ca1de90b6e49dd581e6839d39684092796b61605
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-01-22 13:16:26 -0500

    removed services

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react' ;
 import { BiHome, BiMessageDetail } from 'react-icons/bi' ;
 import { SiAboutdotme } from 'react-icons/si' ;
 import { MdOutlineComputer } from 'react-icons/md' ;
-import { RiServiceLine } from 'react-icons/ri' ;
 import { MdOutlineFolderSpecial } from 'react-icons/md' ;
 
 import './navbar.css' ;
@@ -15,7 +14,6 @@ const Navbar = () => {
             <a href="#home" onClick={ () => setActiveNav('#home') } className={ activeNav === '#' ? 'active' : '' }><BiHome /></a>
             <a href="#about" onClick={ () => setActiveNav('#about') } className={ activeNav === '#about' ? 'active' : '' }><SiAboutdotme /></a>
             <a href="#experience" onClick={ () => setActiveNav('#experience') } className={ activeNav === '#experience' ? 'active' : '' }><MdOutlineComputer /></a>
-            <a href="#services" onClick={ () => setActiveNav('#services') } className={ activeNav === '#services' ? 'active' : '' }><RiServiceLine /></a>
             <a href="#portfolio" onClick={ () => setActiveNav('#portfolio') } className={ activeNav === '#portfolio' ? 'active' : '' }><MdOutlineFolderSpecial /></a>
             <a href="#contact" onClick={ () => setActiveNav('#contact') } className={ activeNav === '#contact' ? 'active' : '' }><BiMessageDetail /></a>
         </nav>
